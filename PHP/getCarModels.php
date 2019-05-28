9980e7e626e0dfc0290d794138e976d681ad280a
ChrisLubin <chrisx700@gmail.com>
2019-05-28 09:20:54 -0400

    Cleanup

--- PHP/getCarModels.php
+++ PHP/getCarModels.php
@@ -1,21 +1,21 @@
 <?php
-    require("db_connect.php");
+  require("db_connect.php");
 
-    $year = mysqli_real_escape_string($db, $_POST['year']);
-    $make = mysqli_real_escape_string($db, $_POST['make']);
+  $year = mysqli_real_escape_string($db, $_POST['year']);
+  $make = mysqli_real_escape_string($db, $_POST['make']);
 
-    $sql = "SELECT DISTINCT model FROM CarInfo WHERE year = $year AND make = '$make' ORDER BY model ASC";
-    $models = array();
+  $sql = "SELECT DISTINCT model FROM CarInfo WHERE year = $year AND make = '$make' ORDER BY model ASC";
+  $models = array();
 
-    $result = mysqli_query($db, $sql);
-    if (mysqli_num_rows($result) > 0) {
-        // Output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            $models[] = array('model' => $row['model']);
-        }
+  $result = mysqli_query($db, $sql);
+  if (mysqli_num_rows($result) > 0) {
+    // Output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+      $models[] = array('model' => $row['model']);
     }
-    
-    echo json_encode($models, JSON_PRETTY_PRINT);
+  }
 
-    $db->close();
+  echo json_encode($models, JSON_PRETTY_PRINT);
+
+  $db->close();
 ?>
